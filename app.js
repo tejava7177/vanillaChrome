c0bdcbda782355af291b0bd835bef937bca205ce
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-11-15 23:24:57 +0900

    commit
    DataType / objects

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-alert("hi");
+// alert("java scipt");
+
+console.log(1234);      //log == print
+
+console.log("hello");
+
+//variable 
+//const 상수
+const a = 5;
+const b = 4;
+let c = 3;
+
+console.log(a+b);
+
+const myName = "juheun";
+console.log(myName);
+
+
+//let 변수
+c = 10;
+console.log(c+a);
+
+
+//array
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fir";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+const nonsense = [1, 2, "hello", null, undefined];
+
+console.log(daysOfWeek, nonsense);
+console.log(daysOfWeek[2]);
+
+
+//add one more day to the array
+daysOfWeek.push("holiday");
+console.log(daysOfWeek);
+
+
+const player = {
+    name: "nico",
+    point: 10,
+    fat: true,
+};
+
+console.log(player);
+console.log(player.name);
+
+player.fat = false;
+player.lastName = "shim";
+player.point += 14;
+
+console.log(player);
