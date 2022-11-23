02016828786835f6cc3c746694467139132bc650
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-11-23 22:36:07 +0900

    commit
    Events / preventDefault

--- Login/app.js
+++ Login/app.js
@@ -1,13 +1,44 @@
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
 
-function btnClick(){
+const HIDDEN_CLASSNAME = "hidden"
+//const loginButton = loginForm.querySelector("button");
+
+/*
+function btnClick() {
     // console.dir(loginInput);
     // console.log("click!");
     console.log(loginInput.value);
+    const username = loginInput.value;
+    if (username === "") {
+        alert("Please write your name");
+    } else if (username.length > 15) {
+        alert("Your name is too long");
+    }
+}
+*/
 
+function onLoginSubmit(event){
+    event.preventDefault();
+    //const username = loginInput.value;
+    // console.log(loginInput.value);
+    loginForm.classList.add("hidden");
+    const username = loginInput.value;
+    //greeting.innerText = "hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    console.log("username");
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+    alert("click");
+}
 
-loginButton.addEventListener("click", btnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
+//loginButton.addEventListener("click", btnClick);
+
