b31e020287e0b018f8098f5fc33ef67567d7e632
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-11-26 01:02:43 +0900

    commit

---
+++ Login/js/weather.js
@@ -0,0 +1,26 @@
+
+
+const API_KEY = "d6c78c91e436886fcf786147dd39b2e4";
+
+
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+    console.log(url);
+    fetch(url).then((response) => response.json()).then((data) => {
+        //console.log(data.name, data.weather[0].main);
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError(){
+    alert("Can't find you, No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
