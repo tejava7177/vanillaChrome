5713a46afc89dfdc5fb0dd6ae6f6019d1b7ac5d0
심주흔 <88835746+tejava7177@users.noreply.github.com>
2022-11-25 22:05:58 +0900

    commit

---
+++ Login/js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h2#clock");
+
+
+function getClock(){
+    const date = new Date();
+    clock.innerText = (`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`);
+}
+
+getClock();
+//setInterval(sayHello, 5000);
+setInterval(getClock, 1000);
+
